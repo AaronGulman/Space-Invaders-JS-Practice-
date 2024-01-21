9581cf8176b4c7bb3d3dead659458a2040d58c73
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-01-21 18:35:22 -0500

    Add a player (.shooter) with a moveShooter function enabling horizontal movement within the game board

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const grid  = document.querySelector('.grid')
+let currenShooterIndex = 202;
+let width = 15;
 
 for(let i = 0; i < 225; i++){
 	const square = document.createElement('div')
 	grid.appendChild(square)
 }
 
-const squares = Array.from(document.querySelector('.grid div'))
+const squares = Array.from(document.querySelectorAll('.grid div'))
 
 const alienInvaders = [
 	0,1,2,3,4,5,6,7,8,9,
@@ -14,7 +16,27 @@ const alienInvaders = [
 ]
 
 function draw(){
-	for(let i = 0; i < alienInvaders; i++){
-		squares[alienInvader[i]].classList.add('invader')
+	for(let i = 0; i < alienInvaders.length; i++){
+		squares[alienInvaders[i]].classList.add('invader')
 	}
 }
+
+draw()
+
+
+
+function moveShooter(e){
+	squares[currenShooterIndex].classList.remove('shooter')
+	switch(e.key){
+		case 'ArrowLeft':
+			if(currenShooterIndex % width !== 0) currenShooterIndex -=1;
+			break;
+		case 'ArrowRight':
+			if(currenShooterIndex % width < width -1) currenShooterIndex +=1;
+			break;
+	}
+	squares[currenShooterIndex].classList.add('shooter')
+
+}
+
+document.addEventListener('keydown',moveShooter)
