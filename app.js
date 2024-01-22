b059576c21f0f5715b9c8914bf460531e720a326
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-01-21 21:02:53 -0500

    Add collision detection that results in 'Game Over'

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const grid  = document.querySelector('.grid')
+const resultsDisplay = document.querySelector('#results')
 let currenShooterIndex = 202;
 let width = 15;
+let direction = 1;
+let invadersId
+let goingRight = true;
 
 for(let i = 0; i < 225; i++){
 	const square = document.createElement('div')
@@ -23,6 +27,14 @@ function draw(){
 
 draw()
 
+function remove(){
+	for(let i = 0; i < alienInvaders.length; i++){
+		squares[alienInvaders[i]].classList.remove('invader')
+	}
+}
+
+squares[currenShooterIndex].classList.add('shooter')
+
 
 
 function moveShooter(e){
@@ -40,3 +52,60 @@ function moveShooter(e){
 }
 
 document.addEventListener('keydown',moveShooter)
+
+
+function moveInvaders(){
+	const leftEdge = alienInvaders[0] % width === 0;
+	const rightEdge = alienInvaders[alienInvaders.length -1] % width === width -1;
+
+	remove()
+
+	if(rightEdge && goingRight){
+		for(let i = 0; i < alienInvaders.length;i++){
+			alienInvaders[i] += width +1;
+			direction = -1;
+			goingRight = false;
+		}
+	}
+
+	if(leftEdge && !goingRight){
+		for(let i = 0; i < alienInvaders.length;i++){
+			alienInvaders[i] += width -1
+			direction = 1;
+			goingRight = true
+		}
+	}
+
+
+	for(let i=0; i < alienInvaders.length;i++){
+		alienInvaders[i] += direction;
+	}
+
+	draw()
+
+	if(squares[currenShooterIndex].classList.contains('invader','shooter')){
+		resultsDisplay.innerHTML = 'Game over'
+		clearInterval(invadersId)
+	}
+
+	for(let i = 0; i < alienInvaders.length; i++){
+		if(alienInvaders[i] > squares.length + width){
+		resultsDisplay.innerHTML = 'Game over'
+		clearInterval(invadersId)
+		}
+	}
+}
+
+
+invadersId = setInterval(moveInvaders,10)
+
+function shoot(e){
+	let laserId
+	let currentLaserIndex = currenShooterIndex;
+	
+	function moveLaser(){
+		squares[currentLaserIndex].classList.remove('laser')
+		currentLaserIndex -= width 
+		squares[currentLaserIndex].classList.add('laser')
+	}
+}
